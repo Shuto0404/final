9fbab7b1752dc2749aa2b0ed47689844f0d1fb2e
Shuto0404 <148170600+Shuto0404@users.noreply.github.com>
2024-01-10 10:57:13 +0900

    Create db-connect.php

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517806-final';
+    const USER = 'LAA1517806';
+    const PASS = 'Pass0404';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
