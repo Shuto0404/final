50f035cedc7924e8a787f238135b265cc0edd6ef
Shuto0404 <148170600+Shuto0404@users.noreply.github.com>
2024-01-19 13:46:14 +0900

    Create touroku_category.js

---
+++ src/script/touroku_category.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    $('input').on('focusin', function() {
+        $(this).parent().find('label').addClass('active');
+    });
+  
+    $('input').on('focusout', function() {
+        if (!this.value) {
+            $(this).parent().find('label').removeClass('active');
+        }
+    });
+  });
+
+  $(".custom-select").each(function() {
+    var classes = $(this).attr("class"),
+        id      = $(this).attr("id"),
+        name    = $(this).attr("name");
+    var template =  '<div class="' + classes + '">';
+        template += '<span class="custom-select-trigger">' + $(this).attr("placeholder") + '</span>';
+        template += '<div class="custom-options">';
+        $(this).find("option").each(function() {
+          template += '<span class="custom-option ' + $(this).attr("class") + '" data-value="' + $(this).attr("value") + '">' + $(this).html() + '</span>';
+        });
+    template += '</div></div>';
+    
+    $(this).wrap('<div class="custom-select-wrapper"></div>');
+    $(this).hide();
+    $(this).after(template);
+  });
+  $(".custom-option:first-of-type").hover(function() {
+    $(this).parents(".custom-options").addClass("option-hover");
+  }, function() {
+    $(this).parents(".custom-options").removeClass("option-hover");
+  });
+  $(".custom-select-trigger").on("click", function() {
+    $('html').one('click',function() {
+      $(".custom-select").removeClass("opened");
+    });
+    $(this).parents(".custom-select").toggleClass("opened");
+    event.stopPropagation();
+  });
+  $(".custom-option").on("click", function() {
+    $(this).parents(".custom-select-wrapper").find("select").val($(this).data("value"));
+    $(this).parents(".custom-options").find(".custom-option").removeClass("selection");
+    $(this).addClass("selection");
+    $(this).parents(".custom-select").removeClass("opened");
+    $(this).parents(".custom-select").find(".custom-select-trigger").text($(this).text());
+  });
+
+  // セレクトボックス
+  var slc_elm = document.querySelector("#slc");
+
+slc_elm.addEventListener("focus", function(elm){
+    if(elm.currentTarget.options.length >= 11){
+        elm.currentTarget.size = "10";
+    }
+}, false);
+
+slc_elm.addEventListener("blur", function(elm){
+    elm.currentTarget.size = "1";
+}, false);
+
+slc_elm.addEventListener("change", function(elm){
+    elm.currentTarget.blur();
+}, false);
