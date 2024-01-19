c3dc442608cc51745d760e1a121e970a7acb6519
Shuto0404 <148170600+Shuto0404@users.noreply.github.com>
2024-01-19 13:46:09 +0900

    Update touroku.php

--- src/touroku.php
+++ src/touroku.php
@@ -5,17 +5,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link href=css/touroku.css rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="script/touroku.js"></script>
 </head>
 <body>
 
-    <h1 class=1>登録画面</h1><br>
-    <form action="home_insert.php" method="post">
-        曲名<input type="text" name="music_name" ><br>
-        アーティスト<input type="text" name="category" ><br>
-        動画リンク<input type="text" name="picture" ><br>
-        <!-- 動画URL<input type="text" name="URL" ><br> -->
-   
-    <button class=sousin>登録</button>
-    </form>
+    <h2>登録画面</h2><br>
+    <div class="setumei">
+    操作説明<br>
+    ・曲名とアーティスト名を入力する<br>
+    ・動画リンクはYoutubeの動画共有から<br>
+    　埋め込むを選択してhttpsから始まる<br>
+    　リンクをコピーして貼り付ける
+    </div>
+    <div class="wrap">
+        <form action="home_insert.php" method="post">
+            <div>
+                <label for="fname">曲名</label>
+                <input type="text"  id="fname" name="music_name" class="cool"/>
+            </div>
+            <div>
+                <label for="lname">アーティスト名</label>
+                <input type="text"  id="lname" name="category" class="cool"/>
+            </div>
+            <div>
+                <label for="email">動画リンク</label>
+                <input type="text"  id="email" name="picture" class="cool"/>
+            </div>
+            <!-- 動画URL<input type="text" name="URL" ><br> -->
+    
+        <button class=sousin>登録</button>
+        </form>
+    </div>
 </body>
 </html>
